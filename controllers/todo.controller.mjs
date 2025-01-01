8a8c49f4a0e1b421418b693523715075d91d02ea
Ricardo <ricardobassanesi@gmail.com>
2025-01-01 18:12:25 -0300

    fim secao 3

--- controllers/todo.controller.mjs
+++ controllers/todo.controller.mjs
@@ -1,1 +1,6 @@
-export const createTodo = () => {};
+import TodoModel from "../model/todo.model.mjs";
+
+export const createTodo = (req, res, next) => {
+  const createdModel = TodoModel.create(req.body);
+  res.status(201).send().json(createdModel);
+};
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,8 +1,44 @@
 import { createTodo } from "../../controllers/todo.controller.mjs";
-// const TodoController = require("../../controllers/todo.controller");
+import TodoModel from "../../model/todo.model.mjs";
+import httpMocks from "node-mocks-http";
+
+import newTodo from "../mock-data/new-todo.json";
+
+//acho que isso é o retorno, não o teste em si
+TodoModel.create = jest.fn();
+
+//variáveis aqui, para não ficafem presas no escopo
+let req, res, next;
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe("TodoController.createTodo", () => {
+  beforeEach(() => {
+    req.body = newTodo;
+  });
+
   it("should have a createTodo function", () => {
     expect(typeof createTodo).toBe("function");
+  });
+
+  it("should call TodoModel.create", () => {
+    createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
+  });
+
+  it("should return 201 response code", () => {
+    createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return json body in response", () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    createTodo(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newTodo);
   });
 });
